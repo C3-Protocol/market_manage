1f0397baa5e85786015286f62796c1ecf1a80517
edddyguo <edddyguo@gmail.com>
2022-06-15 17:15:37 +0800

    update config path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 #nohup sudo ./webhook -hooks hooks.json -verbose > hooks.log 2>&1
-su - feilian -c "cd /home/feilian/repo/market_manage;git pull"
+su - feilian -c "cd /home/Leven/market_manage;git pull"
 sudo nginx -s reload
 
